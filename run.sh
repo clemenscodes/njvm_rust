bf86439e746c15d281d37ab5168f5f749c753e29
Clemens Horn <clemens.horn@mni.thm.de>
2022-01-28 09:45:07 +0100

    aufgabe 0 done
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+cargo build --release
+cp target/release/njvm njvm
--- src/cli/start_cli.rs
+++ src/cli/start_cli.rs
@@ -1,6 +1,35 @@
 extern crate lib;
 use std::env;
+use std::process::exit;
+
+pub fn print_help() {
+    println!("usage: ./njvm [option] [option] ...");
+    println!("  --version        show version and exit");
+    println!("  --help           show this help and exit");
+    exit(0);
+}
+
+pub fn print_version() {
+    println!("Ninja Virtual Machine version 0 (compiled Sep 23 2015, 10:36:52)");
+    exit(0);
+}
+
+pub fn print_err(arg: &str) {
+    println!("unknown command line argument '{arg}', try './njvm --help'");
+    exit(0);
+}
 
 pub fn start_cli() {
-    println!("{:?}", env::args())
+    let args = env::args().skip(1);
+    for arg in args {
+        if arg == "--help" {
+            print_help()
+        }
+        if arg == "--version" {
+            print_version()
+        }
+        print_err(&arg)
+    }
+    println!("Ninja Virtual Machine started");
+    println!("Ninja Virtual Machine stopped");
 }
