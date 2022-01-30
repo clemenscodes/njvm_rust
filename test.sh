cd0b74515d168692eaf7cdc61165135889fa740d
Clemens Horn <clemens.horn@mni.thm.de>
2022-01-30 16:39:11 +0100

    add test script to simulate pipeline
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@
 
 cargo fmt
 cargo clippy --release --target x86_64-unknown-linux-gnu -- -D warnings
+cargo test --workspace --release --target x86_64-unknown-linux-gnu
+cargo build --release --target x86_64-unknown-linux-gnu --locked
