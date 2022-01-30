cafb93d4f3ce4c087ed4616bb2fea8e34ec8913d
Clemens Horn <clemens.horn@mni.thm.de>
2022-01-30 16:42:25 +0100

    update test script
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cargo fmt
+cargo fmt --all -- --check
 cargo clippy --release --target x86_64-unknown-linux-gnu -- -D warnings
 cargo test --workspace --release --target x86_64-unknown-linux-gnu
 cargo build --release --target x86_64-unknown-linux-gnu --locked
+
+cp target/x86_64-unknown-linux-gnu/release/njvm njvm
