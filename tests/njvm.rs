889dd9270a65e025dbe283d47d3d0fafc87e968f
Clemens Horn <clemens.horn@mni.thm.de>
2022-01-31 11:12:41 +0100

    add tests for programs
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tests/njvm.rs
+++ tests/njvm.rs
@@ -1,6 +1,14 @@
 use anyhow::Result;
 use assert_cmd::{crate_name, Command};
+use difference::Changeset;
 use predicates::str::contains;
+
+#[test]
+pub fn njvm_works() -> Result<()> {
+    let mut cmd = Command::cargo_bin(crate_name!())?;
+    cmd.assert().success();
+    Ok(())
+}
 
 #[test]
 pub fn help_works() -> Result<()> {
@@ -20,16 +28,60 @@ pub fn unknown_command_fails() -> Result<()> {
     test_output_failure("--unknown-arg", &output)
 }
 
+#[test]
+pub fn prog1_works() -> Result<()> {
+    let output = read_file("tests/data/prog1.out")?;
+    test_output("--prog1", &output)
+}
+
+#[test]
+pub fn prog2_works() -> Result<()> {
+    let output = read_file("tests/data/prog2.out")?;
+    test_stdin_output("--prog2", "10", &output)
+}
+
+#[test]
+pub fn prog3_works() -> Result<()> {
+    let output = read_file("tests/data/prog3.out")?;
+    test_stdin_output("--prog3", "1", &output)
+}
+
+fn read_file(filename: &str) -> Result<String> {
+    let content = std::fs::read_to_string(filename)?;
+    Ok(content)
+}
+
+fn test_stdin_output(arg: &str, stdin: &str, output: &str) -> Result<()> {
+    let mut cmd = Command::cargo_bin(crate_name!())?;
+    let stdout = cmd.arg(arg).write_stdin(stdin).output()?.stdout;
+    let stdout = String::from_utf8(stdout)?;
+    let output = String::from(output);
+    let changeset = Changeset::new(&output, &stdout, "");
+    println!("{changeset}");
+    assert_eq!(output, stdout);
+    Ok(())
+}
+
+fn test_output(arg: &str, output: &str) -> Result<()> {
+    let mut cmd = Command::cargo_bin(crate_name!())?;
+    let stdout = cmd.arg(arg).output()?.stdout;
+    let stdout = String::from_utf8(stdout)?;
+    let output = String::from(output);
+    let changeset = Changeset::new(&output, &stdout, "");
+    println!("{changeset}");
+    assert_eq!(output, stdout);
+    Ok(())
+}
+
 fn test_output_success(arg: &str, output: &str) -> Result<()> {
     let mut cmd = Command::cargo_bin(crate_name!())?;
     cmd.arg(arg);
     cmd.assert().success().stdout(contains(output));
     Ok(())
 }
-
 fn test_output_failure(arg: &str, output: &str) -> Result<()> {
     let mut cmd = Command::cargo_bin(crate_name!())?;
     cmd.arg(arg);
-    cmd.assert().failure().stderr(contains(output));
+    cmd.assert().failure().code(1).stderr(contains(output));
     Ok(())
 }
