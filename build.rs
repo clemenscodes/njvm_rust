716a17e2d7e6f2b04a835dae3d7c2fdcd1116fb3
Clemens Horn <clemens.horn@mni.thm.de>
2022-07-07 22:05:56 +0200

    link libbigint
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- build.rs
+++ build.rs
@@ -1,8 +1,14 @@
+use std::process::Command;
+
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
     println!("outdir: {}", out_dir);
-    std::process::Command::new("make")
+    Command::new("make")
         .args(&["-C", "lib"])
+        .status()
+        .unwrap();
+    Command::new("cp")
+        .args(&["lib/build/lib/libbigint.a", &out_dir])
         .status()
         .unwrap();
     println!("cargo:rustc-link-search=native={}", out_dir);
