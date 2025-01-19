e892a0d6e913e02d635030786c8585c803876162
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-19 15:32:34 +0100

    fix(bigint): silence bindgen warns

--- src/bigint/mod.rs
+++ src/bigint/mod.rs
@@ -1,1 +1,21 @@
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(clippy::const_static_lifetime)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::cyclomatic_complexity)]
+#![allow(clippy::useless_transmute)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bigint_lib() {
+        unsafe {
+            println!("{:#?}", bip);
+        }
+    }
+}
